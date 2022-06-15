ef15df15f9abe1a5d0c2d38fef7a06c75bed16e2
michealcoolbeans <106983304+michealcoolbeans@users.noreply.github.com>
2022-06-15 14:41:02 +0200

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,21 @@
+﻿function contact() {
+    var script_url = https://script.google.com/macros/s/AKfycbwKt-OR6TBPlYsmajIXqDhY_SNCYvxVfM_exYtOpNCjY7QhikLFNb0VE7Fdz-9pr_M/exec;
+
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var subject = $("#subject").val();
+    var message = $("#message").val();
+
+    var url = script_url + "?callback=thanks&name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email) + "&subject="
+        + encodeURIComponent(subject) + "&message=" + + encodeURIComponent(message);
+    jQuery.ajax({
+        crossDomain: true,
+        url: url,
+        method: "GET",
+        dataType: "jsonp",
+    });
+}
+function thanks(e) {
+    alert("Thank you, I'll respond as soon as possible");
+}
+
